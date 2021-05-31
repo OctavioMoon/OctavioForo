b6b44af8de237d279a52a4c4ef111ccd9d792240
ITSSNP <ITSSNP@PC_OCTAVIO_MOON.lan>
2021-05-31 15:27:25 -0500

    Actualizado

--- src/main/java/com/dwa/foro/controladores/TemasController.java
+++ src/main/java/com/dwa/foro/controladores/TemasController.java
@@ -58,7 +58,7 @@ public class TemasController {
 		if(!servicioTema.agregar(tema));
 			modelo.addAttribute("error", servicioTema.getMensaje());
 		}
-		return "redirect:/temas/listar";
+		return "redirect:temas/listar";
 	}
 	
 	@GetMapping("/temas/detalles/{id}")
@@ -95,7 +95,7 @@ public class TemasController {
 		if(!servicioReplica.agregar(replica));
 			modelo.addAttribute("error", servicioReplica.getMensaje());
 		}
-		return "redirect:/temas/detalles/" + replica.getTema().getId();
+		return "redirect:temas/detalles/" + replica.getTema().getId();
 		}
 	}
 
